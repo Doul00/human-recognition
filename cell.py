afb95092f84445776798032ac4a21c5989e05bb2
Amadou Abdoul-aziz <aabdoul.amadou@gmail.com>
2017-04-17 01:16:35 +0200

    Adding block and feature vector computation

--- cell.py
+++ cell.py
@@ -3,3 +3,8 @@ class Cell:
     def __init__(self, number, histogram):
         self.number = number
         self.histogram = histogram
+
+class Block:
+
+    def __init__(self, histogram):
+        self.histogram = histogram
--- hog.py
+++ hog.py
@@ -1,13 +1,28 @@
 import numpy as np
 import math
 from cell import Cell
+from cell import Block
+
+
+def normalize_vector(vect):
+    total = 0
+    for i in range(len(vect)):
+        total += vect[i] ** 2
+
+    norm = math.sqrt(total)
+    vect = vect / norm
+    return vect
+
+#
+# Computes histogram for a single 8x8 cell
+#
 
 def compute_cell_histogram(magnitude_array, direction_array, offset_x,
-                           offset_y, cell_index):
+                           offset_y, cell_index, cell_size):
 
     histogram = np.zeros((9), np.uint16)
-    for i in range(offset_x, offset_x + 8):
-        for j in range(offset_y, offset_y + 8):
+    for i in range(offset_x, offset_x + cell_size):
+        for j in range(offset_y, offset_y + cell_size):
             grad = direction_array[i][j]
             magnitude = magnitude_array[i][j]
 
@@ -26,24 +41,73 @@ def compute_cell_histogram(magnitude_array, direction_array, offset_x,
 
     return Cell(cell_index, histogram)
 
-def compute_all_histograms(magnitude_array, direction_array):
-    picture_cells = [Cell] * 128
+#
+# Computes histograms for all of the cells
+#
+
+def compute_all_histograms(magnitude_array, direction_array, cell_size):
     cell_index = 0
     offset_x = 0
     offset_y = 0
     rows = magnitude_array.shape[0]
     cols = magnitude_array.shape[1]
+    picture_cells = [Cell] * int((rows / cell_size * cols / cell_size))
 
-    while offset_x <= rows - 8 and offset_y <= cols - 8:
+    while offset_x <= rows - cell_size and offset_y <= cols - cell_size:
         new_cell = compute_cell_histogram(magnitude_array, direction_array,
-                                          offset_x, offset_y, cell_index)
+                                          offset_x, offset_y, cell_index,
+                                          cell_size)
 
         picture_cells[cell_index] = new_cell
         cell_index += 1
-        if offset_y == cols - 8 and offset_x != rows - 8:
+        if offset_y == cols - cell_size and offset_x != rows - cell_size:
             offset_y = 0
-            offset_x += 8
+            offset_x += cell_size
         else:
-            offset_y += 8
+            offset_y += cell_size
 
     return picture_cells
+
+#
+# Creates overlapping blocks with size = cells_per_block * cell_size
+#
+
+def create_blocks(picture_cells, cells_per_block, cell_size, rows, cols):
+    block_number = 0
+    block_array = []
+    cells_per_line = int(cols / cell_size)
+    cells_per_column = int(rows / cell_size)
+
+    # Remove cell_per_block so the block won't go outside of bounds
+    for i in range (cells_per_column - (cells_per_block - 1)):
+        for j in range (cells_per_line - (cells_per_block - 1)):
+            concatenated_vect = np.asarray([])
+            rows_index = i * cells_per_line
+            line_cells = np.asarray([x for x in range(rows_index + j, rows_index + cells_per_block + j)])
+            block_matrix = np.asarray([]).astype(int)
+            block_matrix = np.append(block_matrix, line_cells)
+            down_cells_index = cells_per_line
+
+            # Computes the values for the block matrix
+            for k in range(cells_per_block - 1):
+                block_matrix = np.append(block_matrix, np.copy(line_cells) + down_cells_index)
+                down_cells_index += cells_per_line
+
+            # Gets the histogram for each value in the matrix
+            for index in block_matrix:
+                concatenated_vect = np.append(concatenated_vect, picture_cells[index].histogram)
+
+            concatenated_vect = normalize_vector(concatenated_vect)
+            block_array += [Block(concatenated_vect)]
+            block_number += 1
+    return np.asarray(block_array)
+
+#
+# Creates the final feature vector
+#
+
+def concatenate_blocks(block_array):
+    feature_vector = np.asarray([])
+    for block in block_array:
+        feature_vector = np.append(feature_vector, block.histogram)
+    return feature_vector
