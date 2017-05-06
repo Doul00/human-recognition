f13178c777b5425be5790194d3e4542ddb4d5563
Arthur Douillard <douill_a@epita.fr>
2017-05-06 22:20:16 +0200

    hog: Add hog features visualization.

--- hog/main.py
+++ hog/main.py
@@ -1,6 +1,8 @@
 import time
+import math
 
 from PIL import Image
+from PIL.ImageDraw import Draw
 import numpy as np
 
 from . import preprocessing
@@ -27,6 +29,7 @@ def get_hog_features(img_path, verbose=False):
     logging_begin('Preprocessing', verbose)
     img = preprocessing.grayscale(img)
     img = preprocessing.bilinear_resize(img, 64, 128)
+    original_shape = img.shape
     logging_end(verbose)
 
     logging_begin('Computing hog', verbose)
@@ -41,5 +44,89 @@ def get_hog_features(img_path, verbose=False):
     logging_begin('DONE in {}s'.format(str(round(time.time() - begin_time, 3))),
                   verbose)
 
+
+    visualize_hog(hog_feature_vector, img)
     return hog_feature_vector
 
+
+def visualize_hog(hog_features, img):
+    dim_width  = img.shape[1]
+    dim_height = img.shape[0]
+    img = Image.fromarray(img)
+
+    cell_size = 8
+    bin_size  = 9
+    rad_range = math.pi / 9
+
+    nb_width  = dim_width // cell_size
+    nb_height = dim_height // cell_size
+
+    gradients_strength = [[[.0 for _ in range(bin_size)]
+                           for _ in range(nb_width)]
+                          for _ in range(nb_height)]
+
+    cell_update_counter = [[0 for _ in range(nb_width)]
+                           for _ in range(nb_height)]
+
+    hog_index = 0
+
+    for block_w in range(nb_width - 1):
+        for block_h in range(nb_height - 1):
+            for cell in range(4):
+                cell_w = block_w
+                cell_h = block_h
+                if cell == 1:
+                    cell_h += 1
+                elif cell == 2:
+                    cell_w += 1
+                elif cell == 3:
+                    cell_w += 1
+                    cell_h += 1
+
+                for b in range(bin_size):
+                    gradient_strength = hog_features[hog_index]
+                    hog_index += 1
+                    gradients_strength[cell_h][cell_w][b] += gradient_strength
+
+                cell_update_counter[cell_h][cell_w] += 1
+
+
+    for cell_w in range(nb_width):
+        for cell_h in range(nb_height):
+            nb_update = cell_update_counter[cell_h][cell_w]
+
+            for b in range(bin_size):
+                gradients_strength[cell_h][cell_w][b] /= nb_update
+
+
+    draw = Draw(img)
+    for cell_w in range(nb_width):
+        for cell_h in range(nb_height):
+            draw_x = cell_w * cell_size
+            draw_y = cell_h * cell_size
+
+            my_x = draw_x + cell_size / 2
+            my_y = draw_y + cell_size / 2
+
+            """
+            draw.rectangle([(draw_x, draw_y),
+                            (draw_x+cell_size, draw_y+cell_size)],
+                           outline=128)
+            """
+
+            for b in range(bin_size):
+                grad = gradients_strength[cell_h][cell_w][b]
+                if grad == 0:
+                    continue
+
+                rad = b * rad_range + rad_range/2
+                max_vec_len = cell_size/2
+
+                x0 = my_x - rad * grad * max_vec_len
+                y0 = my_y - rad * grad * max_vec_len
+                x1 = my_x - rad * grad * max_vec_len
+                y1 = my_y - rad * grad * max_vec_len
+
+                draw.line([(x0, y0), (x1, y1)], fill=128, width=3)
+
+    img.show()
