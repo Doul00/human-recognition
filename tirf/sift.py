sift: Fix incorrect calculation of keypoints in DOG.

tirf/sift.py
@@ -4,6 +4,13 @@ import numpy as np
 from scipy import misc
 from scipy.ndimage import filters
 
+
+def create_sift_features(img):
+    """
+    See: http://docs.opencv.org/trunk/da/df5/tutorial_py_sift_intro.html
+    """
+    print(harris_score(img))
+
 def compute_octaves(img, *, sigma=1.6, octave_nb=5):
     """
     Computes @octave_nb octaves with a @sigma starting from 1.6 and growing
@@ -26,6 +33,13 @@ def scale_down(img):
 
 
 def difference_of_gaussian(img):
+    """
+    Computes the Difference Of Gaussian:
+        Several octaves are made, each a gaussian blur of the precedent with
+        sigma starting from 1.6 and growing up by a factor of sqrt(2).
+        Local extrema (max or min) are selected as potential keypoints between
+        several layers of octaves.
+    """
     octaves = compute_octaves(img)
     mid_octave = len(octaves) // 2
     keypoints = [] # Are made of tuples of coordinates (x, y)
@@ -46,17 +60,44 @@ def difference_of_gaussian(img):
                     coord_y = y + yy
                     coord_x = x + xx
                     for octave in octaves:
-                        print('t: ', octave.item(coord_y, coord_x, 0))
                         min_extrema = min(min_extrema,
                                           octave.item(coord_y, coord_x, 0))
-                        max_extrema = min(max_extrema,
+                        max_extrema = max(max_extrema,
                                           octave.item(coord_y, coord_x, 0))
 
-                        print(min_extrema, max_extrema)
-            exit()
+
             if potential_keypoint >= max_extrema or\
                potential_keypoint <= min_extrema:
                 keypoints.append((x, y))
 
 
     print(len(keypoints))
+
+
+def harris_score(img, *, sigma=1.6):
+    """
+    Computes the harris score (from the harris corner detection algorithm)
+    for each pixel.
+    See for all the related notation:
+        http://aishack.in/tutorials/harris-corner-detector
+    """
+    # First we are computing the derivating for each axis (x, and y) found
+    # with the Taylor series.
+    i_x = filters.gaussian_filter(img, sigma=sigma, order=0)
+    i_y = filters.gaussian_filter(img, (sigma,sigma), (1,0))
+
+    i_xx = filters.gaussian_filter(i_x * i_x, sigma)
+    i_yy = filters.gaussian_filter(i_y * i_y, sigma)
+    i_xy = filters.gaussian_filter(i_x * i_y, sigma)
+
+    # We are now computing the score R for a window with the eigenvalues of
+    # the matrix:
+    #   M = [ Ix^2  IxIy ]
+    #       [ IxIy  Iy^2 ]
+    # With R = det M - trace(M)^2
+
+    det_M = i_xx * i_yy - i_xy ** 2
+    tr_M  = i_xx + i_yy
+
+    return det_m - tr_m ** 2
+